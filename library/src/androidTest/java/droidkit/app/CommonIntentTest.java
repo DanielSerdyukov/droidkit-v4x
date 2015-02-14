9f573fd3e1d9a17e09acf2cccf5ff88ce3c431de
Daniel Serdyukov <dvp.troy@gmail.com>
2015-02-14 17:07:33 +0300

    rollback to junit4

--- library/src/androidTest/java/droidkit/app/CommonIntentTest.java
+++ library/src/androidTest/java/droidkit/app/CommonIntentTest.java
@@ -1,27 +1,32 @@
 package droidkit.app;
 
 import android.support.test.InstrumentationRegistry;
-
-import junit.framework.TestCase;
+import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
 /**
  * @author Daniel Serdyukov
  */
-public class CommonIntentTest extends TestCase {
+@RunWith(AndroidJUnit4.class)
+public class CommonIntentTest {
 
-    public void testOpenUrl() throws Exception {
+    @Test
+    public void openUrl() throws Exception {
         Assert.assertFalse(IntentUtils.getResolution(InstrumentationRegistry.getContext(),
                 CommonIntent.openUrl("https://google.com")).isEmpty());
     }
 
-    public void testSearch() throws Exception {
+    @Test
+    public void search() throws Exception {
         Assert.assertFalse(IntentUtils.getResolution(InstrumentationRegistry.getContext(),
                 CommonIntent.search("olololo")).isEmpty());
     }
 
-    public void testSendEmail() throws Exception {
+    @Test
+    public void sendEmail() throws Exception {
         Assert.assertFalse(IntentUtils.getResolution(InstrumentationRegistry.getContext(),
                 CommonIntent.sendEmail(new String[]{
                                 "test@google.com"},
@@ -31,12 +36,14 @@ public class CommonIntentTest extends TestCase {
         ).isEmpty());
     }
 
-    public void testSendSms() throws Exception {
+    @Test
+    public void sendSms() throws Exception {
         Assert.assertFalse(IntentUtils.getResolution(InstrumentationRegistry.getContext(),
                 CommonIntent.sendSms("+1234567890", "Test Passed")).isEmpty());
     }
 
-    public void testOpenDialer() throws Exception {
+    @Test
+    public void openDialer() throws Exception {
         Assert.assertFalse(IntentUtils.getResolution(InstrumentationRegistry.getContext(),
                 CommonIntent.openDialer("+1234567890")).isEmpty());
     }
